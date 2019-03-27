7a7ba8b2b5d246a79111a039aaee0c4a071824b3
Jebiggerstaff <36938415+Jebiggerstaff@users.noreply.github.com>
2019-03-27 10:14:35 -0500

    Added Descriptions onMouseOver

---
+++ Dream Alchemy/Assets/Scripts/NameDisplayController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class NameDisplayController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject text;
+    public GameObject POS;
+    public GameObject NEG;
+
+    public string pos;
+    public string neg;
+
+
+    private void Update()
+    {
+        if (text == null)
+            text = GameObject.Find("Text");
+
+        if (POS == null)
+            POS = GameObject.Find("Pos");
+
+        if (NEG == null)
+            NEG = GameObject.Find("Neg");
+    }
+
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        text.GetComponent<Text>().text = name;
+        POS.GetComponent<Text>().text = pos;
+        NEG.GetComponent<Text>().text = neg;
+
+    }
+
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        text.GetComponent<Text>().text = "";
+        POS.GetComponent<Text>().text = "";
+        NEG.GetComponent<Text>().text = "";
+    }
+}
